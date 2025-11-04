5c99607d48504eedb2094c8cbd54e7d9d15f7a7c
brioengineer <steve@briotech.com>
2025-11-04 11:27:02 -0700

    Refactor firebase-config.js for new Firebase setup
    Updated Firebase configuration to use the latest settings and export the configuration object.

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,11 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// This is the entire, correct content for firebase-config.js
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDlEfOTljgXRZQfn7bH-zoe1LvwdfBeXnk",
-    authDomain: "shortcut-game-4f176.firebaseapp.com",
-    projectId: "shortcut-game-4f176",
-    storageBucket: "shortcut-game-4f176.firebasestorage.app",
-    messagingSenderId: "348992155481",
-    appId: "1:348992155481:web:6e4890ed6d26274173e648",
-    measurementId: "G-TR0NML50HL"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+export const firebaseConfig = {
+  apiKey: "AIzaSyBiigI....(your key)...Luwf8ednak",
+  authDomain: "shortcut-game-4f176.firebaseapp.com",
+  projectId: "shortcut-game-4f176",
+  storageBucket: "shortcut-game-4f176.appspot.com",
+  messagingSenderId: "34892155481",
+  appId: "1:34892155481:web:6a4980edb262f473efd40",
+  measurementId: "G-7H0K4L50FL"
+};
