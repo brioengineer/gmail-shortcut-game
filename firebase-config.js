ca14d7022c7604886de55b292781e128c0a2103a
brioengineer <steve@briotech.com>
2025-11-04 14:43:33 -0700

    Update measurementId in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,5 +7,5 @@ export const firebaseConfig = {
   storageBucket: "shortcut-game-4f176.appspot.com",
   messagingSenderId: "34892155481",
   appId: "1:34892155481:web:6a4980edb262f473efd40",
-  measurementId: "G-7H0K4L50FL"
+  measurementId: "G-TR0NML50HL"
 };
